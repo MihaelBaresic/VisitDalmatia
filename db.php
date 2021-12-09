ef0a23bd0cc6cc29088fa3e3e0ffff31b284bfe7
Mihael Barešić <73283548+MihaelBaresic@users.noreply.github.com>
2021-12-09 14:05:04 +0100

    t

--- db.php
+++ db.php
@@ -1,22 +1,11 @@
 <?php
-$DATABASE_HOST = 'visitdalmatia.database.windows.net';
-$DATABASE_USER = 'dalmatia';
-$DATABASE_PASS = 'visitMe00';
-$DATABASE_NAME = 'visitDalmatia';
-
-$con = mssqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-
-
-
-if(!$con) {
-    echo "Connection failed!";
-}
+    $serverName = "visitdalmatia.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "visitDalmatia", // update me
+        "Uid" => "dalmatia", // update me
+        "PWD" => "visitMe00" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
 ?>
 
-<!-- $host ="xxx.xxx.xxx.xxx"; 
-$username ="username";
-$password ="password";
-$database ="database";
-
-mssql_connect($host, $username, $password);
-mssql_select_db($database); -->

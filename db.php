3ae5a8bf52f14e79cb2fffcd6e3443e55b9a2842
ivan <icalet00@fesb.hr>
2022-01-05 12:50:57 +0100

    ..

--- db.php
+++ db.php
@@ -1,16 +1,4 @@
 <?php
-<<<<<<< HEAD
-    $serverName = "visitdalmatia.database.windows.net"; // update me
-    $connectionOptions = array(
-        "Database" => "visitDalmatia", // update me
-        "Uid" => "dalmatia", // update me
-        "PWD" => "visitMe00" // update me
-    );
-    //Establishes the connection
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-?>
-
-=======
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
@@ -24,4 +12,4 @@ if(!$con) {
     echo "Connection failed!";
 }
 ?>
->>>>>>> bd49a1b39efe2b1ab710add6dc746d08466d8a35
+
